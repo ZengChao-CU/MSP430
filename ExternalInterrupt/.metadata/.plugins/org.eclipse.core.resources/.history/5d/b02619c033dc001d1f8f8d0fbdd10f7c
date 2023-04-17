2e60f0f66766b7b93faf2c29153116e5c53bc51c
ZengChao <1994258932@qq.com>
2023-04-17 23:18:06 +0800

    ExternalInterrupt

---
+++ ExternalInterrupt/.metadata/.plugins/org.eclipse.core.resources/.history/5d/b02619c033dc001d1f8f8d0fbdd10f7c
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../HARDWARE/led.c 
+
+C_DEPS += \
+./HARDWARE/led.d 
+
+OBJS += \
+./HARDWARE/led.obj 
+
+OBJS__QUOTED += \
+"HARDWARE\led.obj" 
+
+C_DEPS__QUOTED += \
+"HARDWARE\led.d" 
+
+C_SRCS__QUOTED += \
+"../HARDWARE/led.c" 
+
+
---
+++ ExternalInterrupt/Debug/HARDWARE/delay.d
@@ -0,0 +1,17 @@
+# FIXED
+
+HARDWARE/delay.obj: ../HARDWARE/delay.c
+HARDWARE/delay.obj: ../HARDWARE/delay.h
+HARDWARE/delay.obj: F:/Ti/CCS/ccsv8/ccs_base/msp430/include/msp430.h
+HARDWARE/delay.obj: F:/Ti/CCS/ccsv8/ccs_base/msp430/include/msp430f5529.h
+HARDWARE/delay.obj: F:/Ti/CCS/ccsv8/ccs_base/msp430/include/in430.h
+HARDWARE/delay.obj: F:/Ti/CCS/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h
+HARDWARE/delay.obj: F:/Ti/CCS/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h
+
+../HARDWARE/delay.c: 
+../HARDWARE/delay.h: 
+F:/Ti/CCS/ccsv8/ccs_base/msp430/include/msp430.h: 
+F:/Ti/CCS/ccsv8/ccs_base/msp430/include/msp430f5529.h: 
+F:/Ti/CCS/ccsv8/ccs_base/msp430/include/in430.h: 
+F:/Ti/CCS/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h: 
+F:/Ti/CCS/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h: 
