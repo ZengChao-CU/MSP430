98ada659a52e3d9584c6fac17536447b0cb1366f
ZengChao <1994258932@qq.com>
2023-04-18 23:35:50 +0800

    Update

---
+++ led/led/HARDWARE/led.h
@@ -0,0 +1,15 @@
+/*
+ * Black_GPIO.h
+ *
+ *  Created on: 2020年10月7日
+ *      Author: PC
+ */
+
+#ifndef GO_LED_H_
+#define GO_LED_H_
+
+#include <msp430.h>
+
+void LED_Init(void);//初始化函数
+
+#endif /* GO_LED_H_ */
