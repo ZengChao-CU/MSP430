41e9e5f0418f300b91d44bb8d109b770d63e2ad4
ZengChao <1994258932@qq.com>
2023-04-21 12:20:38 +0800

    BreathingLights by Timer

---
+++ BreathingLights/BreathingLights/Debug/main.d
@@ -0,0 +1,24 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/CCS_Softeware/ccs/ccs_base/msp430/include/msp430f5529.h
+main.obj: D:/CCS_Softeware/ccs/ccs_base/msp430/include/in430.h
+main.obj: D:/CCS_Softeware/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: D:/CCS_Softeware/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: E:/MSP430Practice/BreathingLights/BreathingLights/HARDWARE/delay.h
+main.obj: D:/CCS_Softeware/ccs/ccs_base/msp430/include/msp430.h
+
+../main.c:
+
+D:/CCS_Softeware/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+D:/CCS_Softeware/ccs/ccs_base/msp430/include/in430.h:
+
+D:/CCS_Softeware/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+D:/CCS_Softeware/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+E:/MSP430Practice/BreathingLights/BreathingLights/HARDWARE/delay.h:
+
+D:/CCS_Softeware/ccs/ccs_base/msp430/include/msp430.h:
+
